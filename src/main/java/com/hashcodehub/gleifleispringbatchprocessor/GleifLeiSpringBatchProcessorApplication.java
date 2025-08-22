8ef7274e66109586f3b867949e838d923e3182d9
sandy <9041128179sandy@gmail.com>
2025-08-22 09:18:44 +0530

    initial commit

---
+++ src/main/java/com/hashcodehub/gleifleispringbatchprocessor/GleifLeiSpringBatchProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.hashcodehub.gleifleispringbatchprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GleifLeiSpringBatchProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GleifLeiSpringBatchProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hashcodehub/gleifleispringbatchprocessor/GleifLeiSpringBatchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashcodehub.gleifleispringbatchprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GleifLeiSpringBatchProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

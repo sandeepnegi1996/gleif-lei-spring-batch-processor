f7ecfbe843542a7ec5454e76d866d3f220ee85cd
sandy <9041128179sandy@gmail.com>
2025-08-22 23:21:54 +0530

    added few more details

--- src/main/java/com/hashcodehub/gleifleispringbatchprocessor/GleifLeiApplication.java
+++ src/main/java/com/hashcodehub/gleifleispringbatchprocessor/GleifLeiApplication.java
@@ -1,7 +1,6 @@
 package com.hashcodehub.gleifleispringbatchprocessor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
